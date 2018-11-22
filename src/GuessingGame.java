8247b743a5a3ff45128a5f2fe782661b20244546
Ilari Silander <ilarisilander@hotmail.com>
2018-11-22 15:44:17 +0000

    Added: Player Can Choose Interval

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -1,7 +1,21 @@
 public class GuessingGame {
     public static void main(String[] args) {
+        String p1;
+        String p2;
 
-        Guesser guesser = new Guesser(0,1000);
+
+        if(args.length == 2) {
+            p1 = args[0];
+            p2 = args[1];
+        }else{
+            p1 = "0";
+            p2 = "1000";
+        }
+
+        int z = Integer.parseInt(p1);
+        int x = Integer.parseInt(p2);
+
+        Guesser guesser = new Guesser(z,x);
 
         guesser.start();
 
